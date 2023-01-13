93330081eb4c58ad46cc07460933465614c72242
Crowdbotics <engineering@crowdbotics.com>
2023-01-13 05:59:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aged-boat-dev-56641",
+  url: "https://aged-boat-dev-56641.botics.co",
+  api: "https://aged-boat-dev-56641.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
